12aa3710464ad4f3440397744b2de7b12eb72490
David Eads <davideads@gmail.com>
2013-05-31 03:30:26 -0500

    Basic data parsing for highcharts.

--- healthstats/static/js/app.js
+++ healthstats/static/js/app.js
@@ -1,6 +1,6 @@
 require.config( {
     paths: {
-        highcharts: "//cdnjs.cloudflare.com/ajax/libs/highcharts/2.3.5/highcharts.min"
+        highcharts: "//code.highcharts.com/highcharts"
     },
     shim: {
         highcharts: {
@@ -13,35 +13,32 @@ require.config( {
 require([ "jquery", "underscore", "highcharts" ],
 function($, _, Highcharts) {
     console.log("Starting our app");
-
+	
     // Get, parse, and visualize data.
     $.getJSON("/healthstats/json/summary.json", function(rawdata) {
         console.log("Data downloaded");
 
-        // This could be simplified but who cares. Loop over each community area.
-        _.each(rawdata, function(community) {
+        // This could be simplified but who cares. Loop over each area area.
+        _.each(rawdata, function(area) {
             data = {
                 "birthrate": [],
                 "lowbirthweight": [],
-                "pretermpct": []
             };
-
 
             // Loop over each key. We"ll exploit the fact time-series data 
             // columns always end in numbers and summary columns don't.
-            _.each(_.keys(community), function(key) {
+            _.each(_.keys(area), function(key) {
                 // Split to output ["lowbirthweight", "2006"] etc
                 var key_parts = key.split("_");
 
                 // If the second key is a number, it"s a year. We use isNan here. Wat.
-                if (!isNaN(key_parts[1])) {
+                if (!isNaN(key_parts[1]) && key_parts[0] in data) {
                     // Push value into correct bucket... keys_parts[0] is
-                    // "lowbirthweight" and community[key] is the value.
-                    data[key_parts[0]].push(community[key])
+                    // "lowbirthweight" and area[key] is the value.
+                    data[key_parts[0]].push(area[key])
                 }
-
-                console.log("Data for " + community.name, data);
             });
+
         });
     });
 });
