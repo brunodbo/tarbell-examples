5fb94c6577b91b39afccd6839abea5457a6b0a55
David Eads <davideads@gmail.com>
2013-06-02 00:10:21 -0500

    fix bug in markdown processor

--- base/config.py
+++ base/config.py
@@ -177,4 +177,6 @@ def linebreaksbr(eval_ctx, value):
 @blueprint.app_template_filter()
 def markdown(value):
     """Run text through markdown process"""
-    return Markup(Markdown.markdown(value))
+    if value:
+        return Markup(Markdown.markdown(value))
+    return None
